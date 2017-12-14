1b5de959a29ffb7ec7720082b36f849530f8d360
Johnny Arguedas <johnnyarguedas@hotmail.com>
2017-12-14 00:17:56 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,3 +9,8 @@ function destructivelyAddElementToBeginningOfArray(chocolateBars, candyString){
   chocolateBars.unshift(candyString);
   return chocolateBars;
 }
+
+
+function addElementToEndOfArray(chocolateBars, candyString){
+  return [...chocolateBars, candyString];
+}
