6233a3f01bbd57bfe094636a807ef2a84c40a7d2
Johnny Arguedas <johnnyarguedas@hotmail.com>
2017-12-14 00:10:16 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 
-vfunction addElementToBeginningOfArray(chocolateBars, candyString){
+function addElementToBeginningOfArray(chocolateBars, candyString){
   return [candyString, ...chocolateBars];
 }
